912befc388caa0cc1b2509f6f42d89d5f8d3fc35
madrodgerflynn <madrodgerflynn@gmail.com>
2023-02-15 17:03:23 -0700

    STARTING RUST PRACTICE

---
+++ rustPractice/target/debug/deps/rustPractice-4db33b64dfb75a61.d
@@ -0,0 +1,5 @@
+c:\Users\Owner\OneDrive\Desktop\practicerust\rustPractice\target\debug\deps\rustPractice-4db33b64dfb75a61.rmeta: src\main.rs
+
+c:\Users\Owner\OneDrive\Desktop\practicerust\rustPractice\target\debug\deps\rustPractice-4db33b64dfb75a61.d: src\main.rs
+
+src\main.rs:
---
+++ rustPractice/target/debug/deps/rustPractice-8bed5e52693854a7.d
@@ -0,0 +1,5 @@
+c:\Users\Owner\OneDrive\Desktop\practicerust\rustPractice\target\debug\deps\rustPractice-8bed5e52693854a7.rmeta: src\main.rs
+
+c:\Users\Owner\OneDrive\Desktop\practicerust\rustPractice\target\debug\deps\rustPractice-8bed5e52693854a7.d: src\main.rs
+
+src\main.rs:
